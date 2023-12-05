a23cfbd9c20da5b8bb6fe305bdb9f6ebcf9440d1
Szymon Nowicki <sznowicki@eskalacja.com>
2023-12-05 23:00:40 +0100

    feat: self-hosted (meilisearch) FTW 🦄

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 import Mustache from "mustache";
 import template from "./template.html";
 import { search } from '../lib/search.js';
+import hash from './version.js';
 
 export const onRequestGet = async (context) => {
 	const { request, env } = context;
@@ -10,13 +11,15 @@ export const onRequestGet = async (context) => {
 	const startTime = Date.now();
 	const list = q ? await search(env, q, p) : [];
 	const doneIn = Date.now() - startTime;
-
+	const hasResults = list.length > 0;
 	const view = {
 		q,
 		title: 'kukei.eu',
 		list,
-		hasResults: list.length > 0,
+		hasResults,
+		noResults: q && !hasResults,
 		doneIn,
+		hash,
 	};
 
 	const html = Mustache.render(template, view);
---
+++ scripts/make-hash.js
@@ -0,0 +1,16 @@
+import { createHash } from 'crypto'
+import { resolve } from 'node:path';
+import { writeFile } from 'node:fs/promises';
+
+const versionHash = createHash('sha256')
+versionHash.update(Date.now().toString());
+const versionId = versionHash.digest('hex')
+
+const main = async () => {
+	const dirname = import.meta.dirname;
+	const content = `export default '${versionId}';`;
+	const path = resolve(dirname, '../functions/version.js');
+	await writeFile(path, content, 'utf-8');
+}
+
+main();
