f000b18dd63bcc79b80fb873fa8786a974a04fe9
Szymon Nowicki <sznowicki@eskalacja.com>
2023-12-17 20:45:25 +0100

    refactor: move mongo stuff to lib

--- functions/status/index.js
+++ functions/status/index.js
@@ -1,63 +1,8 @@
 import Mustache from 'mustache';
 import template from './template.html';
 import { getDefaultViewData } from '../../lib/view.js';
-import {emitPageView} from "../../lib/plausible.js";
-
-const getIndexStats = async (envs) => {
-	const uri = `${envs.ATLAS_URI}/action/find`;
-	const request = await fetch(uri, {
-		method: 'POST',
-		headers: {
-			'api-key': envs.ATLAS_API_KEY,
-			'content-type': 'application/json',
-			'access-control-request-headers': '*',
-		},
-		body: JSON.stringify({
-			collection: envs.ATLAS_COLLECTION,
-			database: envs.ATLAS_DB,
-			dataSource: envs.ATLAS_SOURCE,
-			projection: { url: 1, lastCrawledAt: 1, index: 1}
-		})
-	});
-
-	const response = await request.json();
-
-	return response?.documents ?? [];
-};
-
-export const getUnchecked = async (envs) => {
-	const uri = `${envs.ATLAS_URI}/action/aggregate`;
-	const request = await fetch(uri, {
-		method: 'POST',
-		headers: {
-			'api-key': envs.ATLAS_API_KEY,
-			'content-type': 'application/json',
-			'access-control-request-headers': '*',
-		},
-		body: JSON.stringify({
-			collection: `${envs.ATLAS_COLLECTION}-links`,
-			database: envs.ATLAS_DB,
-			dataSource: envs.ATLAS_SOURCE,
-			pipeline: [
-				{
-					$match: {
-						lastCrawledAt: {$exists: false}
-					},
-				},
-				{
-					$group: {
-						_id: '$source',
-						count: {$sum: 1}
-					}
-				}
-			]
-		})
-	});
-
-	const response = await request.json();
-
-	return response?.documents?.[0]?.count ?? null;
-};
+import {emitPageView} from '../../lib/plausible.js';
+import { getUnchecked, getIndexStats } from '../../lib/mongo.js';
 
 export const onRequestGet = async (context) => {
 	emitPageView(context);
