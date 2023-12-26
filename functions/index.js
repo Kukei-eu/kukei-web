f49e8cee8abd6c403aedfed684bcbab5eb473b35
Szymon Nowicki <sznowicki@eskalacja.com>
2023-12-26 17:56:38 +0100

    Revert "feat: remove mongo analytics (too slow), make CF KV not expire"
    This reverts commit 8a7d4578ea6e11f22d7fb8f1e246bf8438751953.

--- functions/index.js
+++ functions/index.js
@@ -76,8 +76,12 @@ export const onRequestGet = async (context) => {
 			q,
 			JSON.stringify({
 				hasResults,
-			})
+			}),
+			{
+				expirationTtl: 86400, // 24h
+			}
 		);
+		await trackQuery(context.env, { q, hasResults });
 	}
 	const view = {
 		...viewDefaults,
