53ed38688d0183f99acf77bbc41ec1c4877ae8bc
Szymon Nowicki <sznowicki@eskalacja.com>
2024-03-30 11:32:52 +0100

    ci: build pipeline

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,3 @@
 
 # Execute git pull to update the repository
 git pull
-
-# Build Docker images
-docker compose build
-
-# Stop and remove containers
-docker compose down
-
-# Start containers in detached mode
-docker compose up -d
