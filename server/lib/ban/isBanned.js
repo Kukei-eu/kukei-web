1aa67e7e03d9c4c004390488c78a84cdae4ef76a
Szymon Nowicki <sznowicki@eskalacja.com>
2024-04-10 18:58:23 +0200

    feat: ban phrases

--- server/lib/ban/isBanned.js
+++ server/lib/ban/isBanned.js
@@ -1,8 +1,17 @@
 import shortener from './shortener.js';
+import phrases from './phrases.js';
 
 export const isBanned = (query) => {
+	// Check for shorteners
 	if (shortener.includes(query)) {
 		return true;
+	}
+
+	// Check for banned phrases
+	for (const phrase of phrases) {
+		if (query.toLowerCase().includes(phrase)) {
+			return true;
+		}
 	}
 
 	return false;
---
+++ server/lib/ban/phrases.js
@@ -0,0 +1,8 @@
+// Random phrases that are useless for web devs and used by bad bots.
+export default [
+	'powered by',
+	'sponsored by',
+	'Trackback',
+	'Please',
+	'charcoal',
+];
